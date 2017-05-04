0ab645bb4c375a7831eb1eb31fe5c2e69251ce04
Ravi Munde <ravi72munde@gmail.com>
2017-05-04 10:52:19 +0530

    webhook env update (#3)

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var bot;
 
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook("https://mo-map-telegram-base.herokuapp.com/" + bot.token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 }
 else {
   bot = new Bot(token, { polling: true });
