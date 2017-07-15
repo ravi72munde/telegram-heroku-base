6bfdec9b38eac3f65a3a6123166a412e2cf8e8dd
Ravi Munde <ravi72munde@gmail.com>
2017-07-16 01:44:05 +0530

    SFDC entry and reward gif

--- bot.js
+++ bot.js
@@ -1,3 +1,40 @@
+var token = process.env.TOKEN;
+
+var Bot = require('node-telegram-bot-api');
+var bot;
+var request = require("request");
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  console.log('environment url->>>'+process.env.HEROKU_URL);
+}
+else
+{
+  bot = new Bot(token);
+  bot.deleteWebHook();
+  bot = new Bot(token, { polling: true });
+}
+
+console.log('bot server started...');
+
+bot.onText(/\/start/, (msg) => {
+        var keys = [];
+        keys.push({"text":"1","callback_data":"1"});
+        keys.push({"text":"2","callback_data":"2"});
+        keys.push({"text":"3","callback_data":"3"});
+        keys.push({"text":"4","callback_data":"4"});
+        keys.push({"text":"5","callback_data":"5"});
+
+        bot.sendMessage(msg.chat.id, "Are you feeling ok?", {
+                "parse_mode":"Markdown",
+                 "reply_markup": {
+                         "inline_keyboard": [keys]
+                }
+        });
+
+});
+
+
 bot.on("callback_query", (callbackQuery) => {
     console.log(callbackQuery);
     //telegram user id of the user who sent the messge
@@ -34,6 +71,7 @@ function hitMe(userId, rating){
         }
     }
     request(options, function (error,response,body){
+        console.log("response body : " + body)
     });
 }
 
